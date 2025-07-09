7975d7d12f6725b71d4e40464ae0495d49bc8658
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-07-09 18:39:44 +0530

    DSA5

---
+++ DSA5/DSA5.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int i=5;
+// int a=6;
+// int b,c;
+// b=a+(i++);//value of i used is 6, then i gets incremented
+// c=a+(++i);//13 value of i used is 7 as i gets incremented first and then added 
+// cout<<b<<endl;//11
+// cout<<c<<endl;//13
+// cout<<i--<<endl;//7
+// cout<<i++<<endl;//6
+// cout<<++i<<endl;//8
+// cout<<--i<<endl;//7
+// cout<<i<<endl;//7
+// return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"type value of n"<<endl;
+// cin>>n;
+// int sum=0;
+// for(int i=1; i<=n; i++){
+//     sum=sum+i;
+// }
+// cout<<"the value of sum is:"<<sum<<endl;
+// return 0;
+// }
