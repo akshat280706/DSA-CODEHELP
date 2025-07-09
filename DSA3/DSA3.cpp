3f38db5f089406275a66871690c6e6a532c3e735
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-07-09 18:37:08 +0530

    dsa3

--- DSA3/DSA3.cpp
+++ DSA3/DSA3.cpp
@@ -1,2 +1,63 @@
-// checking
-// check2
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"type the value of n"<<endl;
+// cin>>n;
+// int sum=0;
+// int i =1;
+// while (i<=n)
+// {
+//     /* code */
+//     sum=sum+i;
+//     i=i+1;
+// }
+// cout<<"the value of sum is"<<sum<<endl;
+// return 0;
+// }
+
+
+
+//PATTERN 1://
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"type value of n:"<<endl;
+// cin>>n;
+// int i=1;
+// while (i<=n)
+// {
+//     int j=1;
+//      while (j<=n)
+//      {
+//         cout<<"*";
+//         j=j+1;
+//      }
+//      cout<<endl;
+//      i=i+1;
+// }
+// return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"value of n:"<<endl;
+// cin>>n;
+// int i=1;
+// while (i<=n)
+// {
+//     int j=1;
+//     while (j<=n)
+//     {
+//         cout<<i;
+//         j=j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+// }
+// return 0;
+// }
