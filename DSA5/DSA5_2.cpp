c2a2ce6a19ca59863b31a2675b3056ec5536c05d
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-07-17 10:21:28 +0530

    fibonacci

--- DSA5/DSA5_2.cpp
+++ DSA5/DSA5_2.cpp
@@ -1,5 +1,6 @@
 // #fibonacci series:
 // 0,1,1,2,3,5,8,13,21...
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -9,13 +10,13 @@ int main(){
     cout << endl;
 
     int a = 0, b = 1;
-    cout << a << " " << b<<" ";
+    cout << a << " " << b<< " ";
+
     for (int i = 0; i <= n; i++)
     {
         int nextNumber = a + b;
         // cout << a << b << " ";
         cout << nextNumber << " ";
-
         a = b;
         b = nextNumber;
     }
