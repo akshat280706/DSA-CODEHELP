7ba2bc35d93b353dc8bc43553806a2e750de67fe
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-07-27 19:54:55 +0530

    scope

--- DSA5/primeNumber.cpp
+++ DSA5/primeNumber.cpp
@@ -1,30 +1,56 @@
-// primeNumber
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cout << "enter the value of n\n";
-    cin>>n;
+// // primeNumber
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "enter the value of n\n";
+//     cin>>n;
 
-    if(n<=1){
-        cout << n << " is not a prime number";
-        return 0;
-    }
-    bool isPrime=1;
-    for (int i = 2; i < n; i++)
-    {
-        if (n%i==0)
-        {
-            isPrime=0;
-            break;
-        }
+//     if(n<=1){
+//         cout << n << " is not a prime number";
+//         return 0;
+//     }
+//     bool isPrime=1;
+//     for (int i = 2; i < n; i++)
+//     {
+//         if (n%i==0)
+//         {
+//             isPrime=0;
+//             break;
+//         }
         
-    }
-    if (isPrime==0)
-    {
-        cout << n << " is not a prime number";
-    } else{
-        cout << n << " is a prime number";
-    }
-    return 0;
-}
+//     }
+//     if (isPrime==0)
+//     {
+//         cout << n << " is not a prime number";
+//     } else{
+//         cout << n << " is a prime number";
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     for (int i = 0; i <=5; i++)
+//     {
+//         cout << i << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for (int i = 0; i <=5; i++)
+//     {
+//         for (int j = i; j <=5; j++)
+//         {
+//             cout << i << " " << j << endl;
+//         }
+        
+//     }
+//     return 0;
+// }
---
+++ DSA5/scope.cpp
@@ -0,0 +1,20 @@
+// // variables and scope
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 3;
+//     cout << a << endl;
+//     if (true)
+//     {
+//         /* code */ int a=5, b=4;
+//         cout << a << endl;
+//         cout << b << endl;
+//     }
+//     cout << a << endl;
+//     // cout << b << endl;//error cuz b is not defined for this block(scope)
+//     // int a=7 again error
+//     return 0;
+// }
+
+// operator precedence
+// type of same as bodmas(check google)
