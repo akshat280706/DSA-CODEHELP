72f53ebe030820df0b9b536ce20f8621667061fe
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-08-08 18:09:27 +0530

    commit8

---
+++ switch.cpp
@@ -0,0 +1,52 @@
+// switch (expression)
+// {
+// case constant expression:
+//     /* code */
+//     break;
+
+// default:
+//     break;
+// }
+
+// constants can be integers or character value
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // int num = 1;
+//     // char ch = 'a';
+//     char ch1 = 'b';
+//     cout<<endl;
+//     // switch (num)
+//     // switch (ch)
+//     switch (ch1)
+//     {
+//     case 1:
+//         cout << "this is case 1";
+//         break;
+//     case 2:
+//         cout << "this is case 2";
+//         break;
+//     case 'b':
+//         cout << "this is using char";
+//         break;
+//     default:
+//         cout << "this is the default case";
+//         break;
+//     }
+//     return 0;
+// }
+
+// continue is not valid in case of switch statement
+#include<iostream>
+using namespace std;
+int main(){
+
+    return 0;
+}
+
+
+
+
+
+
