61c18b1a30da1f9069bfa2813e6d01f9556227a7
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-08-12 23:48:08 +0530

    problem1

--- switch.cpp
+++ switch.cpp
@@ -38,12 +38,56 @@
 // }
 
 // continue is not valid in case of switch statement
-#include<iostream>
-using namespace std;
-int main(){
 
-    return 0;
-}
+
+// total 1330rs--> how many 100rs,50rs,20rs,1rs notees are needed(using switch)
+
+// 1.Using switch statement:
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int amount;
+//     int currency = 0;
+//     cout<<"enter the amount: "<<endl;
+//     cin >> amount;
+//     int count100 = 0, count50 = 0, count20 = 0, count1 = 0;
+//     if (amount>=100)
+//     {
+//         currency = 100;
+//     }
+//     else if (amount>=50)
+//     {
+//         currency = 50;
+//     }
+//     else if (amount>=20)
+//     {
+//         currency = 20;
+//     }
+//     else if (amount>=1)
+//     {
+//         currency = 1;
+//     }    
+//     switch (currency)
+//     {
+//     case 100:
+//         count100 = amount / 100;
+//         amount = amount % 100;
+//     case 50:
+//         count50 = amount / 50;
+//         amount = amount % 50;
+//     case 20:
+//         count20 = amount / 20;
+//         amount = amount % 20;
+//     case 1:
+//         count1 = amount / 1;
+//         amount = amount % 1;
+//     }
+//     cout << "No of 100rs Notes are: " << count100 << endl;
+//     cout << "No of 50rs Notes are: " << count50 << endl;
+//     cout << "No of 20rs Notes are: " << count20 << endl;
+//     cout << "No of 1rs Notes are: " << count1 << endl;
+//     return 0;
+// }
 
 
 
