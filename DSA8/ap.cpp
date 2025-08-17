aaf5c6589bc12b466e5de6693f1f20a5700d00a2
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-08-17 22:24:42 +0530

    AP

---
+++ DSA8/ap.cpp
@@ -0,0 +1,31 @@
+// AP(arithematic progression)
+// AP= (3*n+7)
+// i/p--->n
+// o/p--> nth term
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+
+    
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int update(int a){
+//     a -= 5;
+//     return a;
+// }
+// int main(){
+//     int a=15;
+//     update(a);
+//     cout<<a;
+//     return 0;
+// }
+
+
+
+
+
