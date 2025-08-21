3b889fa8faab218694ba4850b7c3c5c587a72427
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-08-21 20:08:19 +0530

    MAX_MIN

---
+++ Max_Min.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int getMIN(int num[], int n){
+
+    int min = num[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i]<min)
+        {
+            min = num[i];
+        }
+    }
+    return min;
+}
+int getMax(int num[], int n){
+
+    int max = num[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i]>max)
+        {
+            max = num[i];
+        }
+    }
+    return max;
+}
+int main(){
+    int size;
+    cout<<"enter the size of the array needed: ";
+    cin >> size;
+
+    int num[100];
+
+    //taking input in array:
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+    }
+    cout << endl;
+    cout << "maximum value is: " << getMax(num, size) << endl;
+    cout << "minimum value is: " << getMIN(num, size) << endl;
+
+    return 0;
+}
