dd277e55a2611fd251108fd9cd64067af834fca3
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-10-04 09:52:20 +0530

    DSA11

---
+++ DSA11/time_complexity.cpp
@@ -0,0 +1,21 @@
+// amout of time taken by an algorithm to run as a fxn of length of the input
+// 1. Big O- upper bound
+// 2. Big theta- average case
+// 3. Big Omega- Worst Case
+
+// if two different loops, then add
+// Eg for(...){
+//            }
+// for{
+// }
+// so O(n)+O(n)
+
+// if nested loop, then multiply
+// for  {
+//for{
+//       } 
+//  }   
+// TC= O(n*m)
+
+
+// SPace complexity-- Memory
