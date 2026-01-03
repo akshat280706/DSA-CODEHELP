36162491de643e0cc2f46b7ad9d83cde51bd4e2b
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2026-01-03 16:49:46 +0530

    189 Leetcode

--- 1.LEETCODE/189_rotatearray.cpp
+++ 1.LEETCODE/189_rotatearray.cpp
@@ -1,122 +1,157 @@
-// #include<iostream>
-// using namespace std;
+// // #include<iostream>
+// // using namespace std;
 
-// void leftRotateArray(int arr[], int n, int d){
-//     //d is the number of times to be rotated to
-//     //n is the total size of the array
-//     d = d % n;
-//     int temp[d];
-//     //storing
-//     for (int i = 0; i < d; i++)
-//     {
-//         temp[i] = arr[i];
-//     }
-//     //shifting
-//     for (int i = d; i <n; i++)
-//     {
-//         arr[i - d] = arr[i];
-//     }
-//     //storing temp to original array
-//     for(int i = n - d; i < n; i++){
-//         arr[i] = temp[i - (n - d)];
-//     }
-// }
-//     int main()
-// {
-//     int n;
-//     cout << "Enter the number of elements of the array: ";
-//     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements of the array: " << endl;
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> arr[i];
-//     }
+// // void leftRotateArray(int arr[], int n, int d){
+// //     //d is the number of times to be rotated to
+// //     //n is the total size of the array
+// //     d = d % n;
+// //     int temp[d];
+// //     //storing
+// //     for (int i = 0; i < d; i++)
+// //     {
+// //         temp[i] = arr[i];
+// //     }
+// //     //shifting
+// //     for (int i = d; i <n; i++)
+// //     {
+// //         arr[i - d] = arr[i];
+// //     }
+// //     //storing temp to original array
+// //     for(int i = n - d; i < n; i++){
+// //         arr[i] = temp[i - (n - d)];
+// //     }
+// // }
+// //     int main()
+// // {
+// //     int n;
+// //     cout << "Enter the number of elements of the array: ";
+// //     cin >> n;
+// //     int arr[n];
+// //     cout << "Enter " << n << " elements of the array: " << endl;
+// //     for (int i = 0; i < n; i++)
+// //     {
+// //         cin >> arr[i];
+// //     }
 
-//     int d;
-//     cout << "Enter the number for the array to be left shifted to: ";
-//     cin >> d;
-//     cout << "The new left shifted array is: ";
-//     leftRotateArray(arr, n, d);
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout << arr[i] << "";
-//     }
-//     cout << endl;
+// //     int d;
+// //     cout << "Enter the number for the array to be left shifted to: ";
+// //     cin >> d;
+// //     cout << "The new left shifted array is: ";
+// //     leftRotateArray(arr, n, d);
+// //     for (int i = 0; i < n; i++)
+// //     {
+// //         cout << arr[i] << "";
+// //     }
+// //     cout << endl;
     
-//     return 0;
-// }
+// //     return 0;
+// // }
 
 
-//now optimal approach
-// #include <iostream>
+// //now optimal approach
+// // #include <iostream>
+// // #include<algorithm>
+// // using namespace std;
+
+// // void leftRotate(int arr[], int n, int d){
+// //     d = d % n;
+// //     reverse(arr, arr + d);
+// //     reverse(arr + d, arr + n);
+// //     reverse(arr, arr + n);
+// // }
+
+// // int main(){
+// //     int n;
+// //     cout << "Enter the number of elements of the array: ";
+// //     cin >> n;
+// //     int arr[n];
+// //     cout << "Enter " << n << " elements of the array: " << endl;
+// //     for (int i = 0; i < n; i++)
+// //     {
+// //         cin >> arr[i];
+// //     }
+
+// //     int d;
+// //     cout << "Enter the number for the array to be left shifted to: ";
+// //     cin >> d;
+// //     cout << "The new left shifted array is: ";
+// //     leftRotate(arr, n, d);
+// //     for (int i = 0; i < n; i++)
+// //     {
+// //         cout << arr[i] << " ";
+// //     }
+// //     cout << endl;
+// //     return 0;
+// // }
+
+// //optimal using vectors
+// #include<iostream>
 // #include<algorithm>
+// #include<vector>
+
 // using namespace std;
-
-// void leftRotate(int arr[], int n, int d){
+// void leftRotate(vector<int>&arr, int d){
+//     int n = arr.size();
 //     d = d % n;
-//     reverse(arr, arr + d);
-//     reverse(arr + d, arr + n);
-//     reverse(arr, arr + n);
-// }
 
+//     reverse(arr.begin(), arr.begin() + d);
+//     reverse(arr.begin() + d, arr.begin() + n);
+//     reverse(arr.begin(), arr.begin() + n);
+// }
 // int main(){
 //     int n;
-//     cout << "Enter the number of elements of the array: ";
+//     cout << "enter the number of elements of an array: ";
 //     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements of the array: " << endl;
+
+//     vector<int> arr(n);
+//     cout << "enter " << n << " elements of the array: " << endl;
 //     for (int i = 0; i < n; i++)
 //     {
 //         cin >> arr[i];
 //     }
 
 //     int d;
-//     cout << "Enter the number for the array to be left shifted to: ";
+//     cout << "enter the value to be shifted to: ";
 //     cin >> d;
-//     cout << "The new left shifted array is: ";
-//     leftRotate(arr, n, d);
+//     leftRotate(arr, d);
+//     cout << "the left shifted array is: ";
 //     for (int i = 0; i < n; i++)
 //     {
 //         cout << arr[i] << " ";
 //     }
-//     cout << endl;
 //     return 0;
 // }
 
-//optimal using vectors
+//leetcode 189
 #include<iostream>
-#include<algorithm>
 #include<vector>
-
+#include<algorithm>
 using namespace std;
-void leftRotate(vector<int>&arr, int d){
-    int n = arr.size();
-    d = d % n;
 
-    reverse(arr.begin(), arr.begin() + d);
-    reverse(arr.begin() + d, arr.begin() + n);
-    reverse(arr.begin(), arr.begin() + n);
+void rotateRight(vector<int>&arr, int k){
+    int n = arr.size();
+    k = k % n;
+    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.end());
 }
 int main(){
     int n;
-    cout << "enter the number of elements of an array: ";
+    cout << "enter the number of elements of the array: ";
     cin >> n;
 
     vector<int> arr(n);
-    cout << "enter " << n << " elements of the array: " << endl;
-    for (int i = 0; i < n; i++)
-    {
+    cout << "enter " << n << " elemnts of the array to be rotated right: " << endl;
+    for (int i = 0; i < n; i++){
         cin >> arr[i];
     }
-
-    int d;
+    int k;
     cout << "enter the value to be shifted to: ";
-    cin >> d;
-    leftRotate(arr, d);
-    cout << "the left shifted array is: ";
-    for (int i = 0; i < n; i++)
-    {
+    cin >> k;
+
+    rotateRight(arr, k);
+    cout << "The right shifted array is: ";
+    for (int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
     return 0;
