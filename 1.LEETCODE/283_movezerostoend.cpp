b3f710d68a3692adf0407c7123fe50c1a6b8c5bc
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2026-01-03 20:05:48 +0530

    189_moveZeros

--- 1.LEETCODE/283_movezerostoend.cpp
+++ 1.LEETCODE/283_movezerostoend.cpp
@@ -12,19 +12,19 @@
 //             i = j;
 //             break;
 //         }
-        
+
 //     }
 //     //no zero found
 //     if (i==-1){
 //         return;
 //     }
-//     for (int j = i+1; j < n; j++)
+//     for (int j = i+1; j < n-1; j++)
 //     {
 //         if (arr[j]!=0)
 //         {
 //             swap(arr[j], arr[i]);
+//              i++;
 //         }
-//         i++;
 //     }
 // }
 // int main(){
@@ -48,3 +48,53 @@
 //     return 0;
 // }
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeros(vector<int>&arr, int n)
+{
+    int i = -1;
+    //finding the first zero
+    for (int j = 0; j < n; j++)
+    {
+        if (arr[j]==0)
+        {
+            i = j;
+            break;
+        }
+        
+    }
+    //no zero found
+    if (i==-1){
+        return;
+    }
+    for (int j = i+1; j < n-1; j++)
+    {
+        if (arr[j]!=0)
+        {
+            swap(arr[j], arr[i]);
+            i++;
+        }
+    }
+}
+int main(){
+    int n;
+    cout << "enter the no of elements of the array: ";
+    cin >> n;
+    cout << endl;
+    vector<int> arr(n);
+    cout << "enter " << n << " elements of the array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << endl;
+    moveZeros(arr, n);
+    cout << "the new array is: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
