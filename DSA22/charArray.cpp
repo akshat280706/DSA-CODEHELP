2c823c86315c6d155c0a291ff166e7f5b7e4113a
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2026-02-02 19:24:42 +0530

    charArray1

---
+++ DSA22/charArray.cpp
@@ -0,0 +1,19 @@
+// // one dimensional character array
+// // cin-->stops execution when a space is given in between
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     char name[20];
+//     cout << "enter your name: ";
+//     cin >> name;
+//     //akshat---> akshat gets printed
+//     // chauhan akshat-->> only chauhan gets printed
+
+//     // \0 is null character, execution stops if this is added
+
+//     name[2] = '\0'; //now output will be just ak
+//     cout << "your name is: " << name << endl;
+//     return 0;
+// }
--- DSA78/hashmap.cpp
+++ DSA78/hashmap.cpp
@@ -5,8 +5,6 @@
 // // <key,value>
 
 
-
-
 // #include<iostream>
 // #include<bits/stdc++.h>
 // using namespace std;
@@ -60,3 +58,46 @@
     
 //     return 0;
 // }
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int maxFreq(vector<int> &arr, int n){
+    // unordered_map<int, int> count;
+    // for(int i=0; i<arr.size; i++){
+    //     count[arr[i]]++;
+    // }
+
+    // int maxi=INT_MIN;
+    // int ans = -1;
+    // for(auto i:count){
+    //     if(i.second>maxi){
+    //         maxi = i.second;
+    //         ans = i.first;
+    //     }
+    // }
+
+//     unordered_map<int, int> count;
+//     int maxFreq = 0;
+//     int maxAns = 0;
+
+//     for (int i = 0; i < arr.size(); i++){
+//         count[arr[i]]++;
+//         maxFreq = max(maxFreq, count[arr[i]]);
+//     }
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         if(maxFreq==count[arr[i]]){
+//             maxAns = arr[i];
+//             break;
+//         }
+//     }
+//     return maxAns;
+// }
+
+// int main(){
+//     return 0;
+// }
+
