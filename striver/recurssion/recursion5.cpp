3bb949aae512f291d9a5251de11fc296848429da
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2026-03-19 19:35:41 +0530

    added 5 rec

---
+++ striver/recurssion/recursion5.cpp
@@ -0,0 +1,18 @@
+//multiplr recursion calls
+
+
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n<=1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin >> n;
+    cout << "the ans is: " << fibonacci(n);
+    return 0;
+}
